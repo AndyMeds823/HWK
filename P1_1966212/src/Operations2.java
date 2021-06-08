7805aa6c6721f0bc8970e5f1b5d968e2649cf11b
AndyMeds823 <82612598+AndyMeds823@users.noreply.github.com>
2021-06-08 15:28:20 -0500

    Add files via upload

--- P1_1966212/src/Operations2.java
+++ P1_1966212/src/Operations2.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
-
 public  class Operations2 extends Operations {
-
+	
 	@Override
 	public ArrayList<Account> heightAmountPaidUsers() {
 		Account sub = getAccount().stream().max(Comparator.comparing(num-> num.getamountoPay())).get();
@@ -12,4 +10,19 @@ public  class Operations2 extends Operations {
 		
 	}
 	
+	
+	public void concatUserNames(){
+		String a1="",a2="";
+		//ArrayList<Account> arr =getAccount();
+		for(Account n : getAccount()) {
+			if(n.getCustomer().gettype()=="3G") {
+				
+				a1=n.getCustomer().getuserName();
+			} else if(n.getCustomer().gettype()=="4G") {
+				
+				a2=n.getCustomer().getuserName();
+			}
+		}
+		System.out.println(a1+" : "+a2);
+	}
 }
--- P1_1966212/src/TestPro.java
+++ P1_1966212/src/TestPro.java
@@ -9,9 +9,16 @@ public class TestPro {
 
 			ArrayList<Account> Ac= new ArrayList<Account>();
 			Ac.add(new Account(75,new Customer("GalanT",2008,"4G",1)));
+			Ac.add(new Account(76,new Customer("ChimaT",2016,"3G",2)));
+			
 			
 			Operations2 opera = new Operations2();
 			opera.setCustomer(CustomerList);
 			opera.setAccount(Ac);
+			
+			System.out.println("3G and 4G ");
+			opera.concatUserNames();
+			System.out.println("Highest Paid: ");
+			opera.heightAmountPaidUsers();			
 	}
 }
